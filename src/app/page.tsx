3452dea71140cd2ddeac246a034fb451f7090a70
Suhwan0818 <tnghks1997@naver.com>
2023-08-07 22:52:24 +0900

    💄 로그인 페이지 디자인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { TopNav } from '@/components/main/topNav'
-import Main from '@/components/main/Main'
-import AdSlider from '@/components/main/AdSlider'
+import Main from "@/components/main/Main";
 
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto py-6 px-4">
-      <TopNav />
       <Main />
       <div id="main"></div>
       <div id="item1"></div>
