36dc71d82800735c4ef1b276cc570cdc5c6aa53b
Suhwan0818 <tnghks1997@naver.com>
2023-08-17 15:54:22 +0900

    🚧 댓글 계속 작업 해야함

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXT_PUBLIC_KAKAO_CLIENT_ID:process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
-    }
-}
-
 const removeImports = require("next-remove-imports")();
 
-module.exports = removeImports(nextConfig)
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // fs 모듈이 브라우저에서 사용되지 않도록 empty 설정을 적용합니다.
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
+};
+
+module.exports = removeImports(nextConfig);
